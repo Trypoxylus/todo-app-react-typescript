6e494fc0149b4290f7e64b4d0a16db2f17e649aa
Trypoxylus <samuraisho0123@gmail.com>
2023-12-19 16:32:05 +0900

    change UI design

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import MenuAppBar from "./components/Header";
+import { ThemeProvider } from "@mui/material";
+import theme from "./theme/theme";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+  type Todo = {
+    inputValue: string;
+    id: number;
+    checked: boolean;
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //console.log(e.target.value);
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const newTodo: Todo = {
+      inputValue: inputValue,
+      id: todos.length,
+      checked: false,
+    };
+
+    setTodos([newTodo, ...todos]);
+    setInputValue("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <MenuAppBar />
+        <div className="top">
+          <div className="top-left">
+            <h1>
+              Make your original todo list.<br></br>Start your great journey.
+            </h1>
+            <form onSubmit={(e) => handleSubmit(e)}>
+              <input
+                type="text"
+                onChange={(e) => handleChange(e)}
+                className="inputText"
+              />
+              <input
+                type="submit"
+                value="create"
+                className="submitButton"
+              ></input>
+            </form>
+          </div>
+          <img
+            src="./images/drawkit-transport-scene-1.svg"
+            alt=""
+            width="500px"
+            className="top-img"
+          />
+        </div>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>{todo.inputValue}</li>
+          ))}
+        </ul>
+      </div>
+    </ThemeProvider>
   );
 }
 
